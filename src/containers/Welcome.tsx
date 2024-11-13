3b8f329275f1a6669d2fce4c26e28b23d373f5b4
paulogermanopg <paulogermano.person@outlook.com.br>
2024-11-13 12:03:46 -0300

    feat: login screem and colors

--- src/containers/Welcome.tsx
+++ src/containers/Welcome.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  Easing,
+  TextInput,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {
   heightPercentToDp as hp,
   widthPercentToDp as wp,
 } from '../utils/sizeUtils';
 import COLORS from '../utils/colorUtils';
+import WelcomeButton from '../components/WelcomeButton';
 
 interface WelcomeProps {}
 
 const Welcome = (props: WelcomeProps) => {
   const {} = props;
+
+  const [startClicked, setStartClicked] = useState<boolean>(false);
+  const [buttonFlex, setButtonFlex] = useState<any>(new Animated.Value(1));
+
+  useEffect(() => {
+    if (startClicked) {
+      Animated.timing(buttonFlex, {
+        toValue: 8,
+        duration: 250,
+        useNativeDriver: false,
+        easing: Easing.linear,
+      }).start();
+    } else {
+      Animated.timing(buttonFlex, {
+        toValue: 1,
+        duration: 250,
+        useNativeDriver: false,
+        easing: Easing.linear,
+      }).start();
+    }
+  }, [startClicked]);
+
   return (
     <LinearGradient
       colors={[COLORS.GRADIENT_1, COLORS.GRADIENT_2, COLORS.GRADIENT_3]}
@@ -20,15 +50,43 @@ const Welcome = (props: WelcomeProps) => {
         <Text style={styles.logoTextStyle}>Project</Text>
       </View>
 
-      <View style={styles.buttomWelcome}>
-        <TouchableOpacity
-          style={{
-            backgroundColor: COLORS.ORANGE,
-          }}
-          onPress={() => console.log('ok')}>
-          <Text>Comece Agora</Text>
-        </TouchableOpacity>
-      </View>
+      <Animated.View style={[styles.buttomWelcome, {flex: buttonFlex}]}>
+        {startClicked ? (
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: COLORS.WHITE,
+              borderTopLeftRadius: wp(18),
+            }}>
+            <Text style={styles.titleLogin}>LOGIN</Text>
+
+            <TextInput
+              style={styles.inputStyle}
+              placeholder="E-mail"
+              keyboardType="email-address"
+              placeholderTextColor={COLORS.WHITE}
+            />
+
+            <TextInput
+              style={styles.inputStyle}
+              placeholder="Senha"
+              secureTextEntry
+              placeholderTextColor={COLORS.WHITE}
+            />
+            <WelcomeButton
+              text="Login"
+              onPress={() => setStartClicked(false)}
+              style={styles.styleAux}
+            />
+          </View>
+        ) : (
+          <WelcomeButton
+            text="Comece Agora"
+            onPress={() => setStartClicked(true)}
+            style={styles.styleAux}
+          />
+        )}
+      </Animated.View>
     </LinearGradient>
   );
 };
@@ -36,22 +94,48 @@ const Welcome = (props: WelcomeProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
   topWelcome: {
     flex: 2,
     justifyContent: 'flex-end',
     paddingVertical: hp(10),
+    alignItems: 'center',
   },
   buttomWelcome: {
-    flex: 1,
+    alignSelf: 'center',
   },
   logoTextStyle: {
     color: COLORS.WHITE,
     fontSize: wp(12),
     letterSpacing: wp(4),
     fontFamily: 'Raleway-Italic-VariableFont_wght',
+  },
+  styleAux: {
+    alignSelf: 'center',
+    marginVertical: hp(2),
+  },
+  titleLogin: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    color: COLORS.BLACK,
+    marginVertical: hp(1),
+    fontSize: wp(8),
+    letterSpacing: wp(0.5),
+    fontFamily: 'Raleway-VariableFont_wght',
+  },
+  inputStyle: {
+    borderRadius: wp(8),
+    width: wp(70),
+    height: hp(6),
+    backgroundColor: COLORS.GRADIENT_OPACITY,
+    alignSelf: 'center',
+    textAlign: 'center',
+    marginVertical: hp(2),
+    color: COLORS.BLACK,
+    fontSize: wp(6),
+    letterSpacing: wp(0.5),
+    fontFamily: 'Raleway-VariableFont_wght',
   },
 });
 
--- src/utils/colorUtils.ts
+++ src/utils/colorUtils.ts
@@ -1,7 +1,11 @@
 export default {
     WHITE: '#FFF',
+    BLACK: '#000',
+    WHITE_OPACITY: '#FFFFFF80',
     GRADIENT_1: '#72c2d9',
     GRADIENT_2: '#35a9ad',
     GRADIENT_3: '#03958b',
     ORANGE: '#f48d3c',
+    GRAY: '#0f0f0f',
+    GRADIENT_OPACITY: '#72c2d980',
 };
